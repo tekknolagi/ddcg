bee0e879bc29101d29171698432eb0b29befd2b4
Max Bernstein <max@bernsteinbear.com>
2022-11-14 01:35:00 -0800

    Initial DDCG: add interp.cpp
    paper: https://legacy.cs.indiana.edu/~dyb/pubs/ddcg.pdf
    slides about old v8: https://raw.githubusercontent.com/eatonphil/one-pass-code-generation-in-v8/main/One-pass%20Code%20Generation%20in%20V8.pdf

---
+++ interp.cpp
@@ -0,0 +1,130 @@
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+
+enum class ExprType {
+  kIntLit,
+  kAddExpr,
+  kVarRef,
+  kAssign,
+};
+
+struct Expr {
+  explicit Expr(ExprType type) : type(type) {}
+  ExprType type;
+};
+
+struct IntLit : public Expr {
+  explicit IntLit(int value) : Expr(ExprType::kIntLit), value(value) {}
+  int value;
+};
+
+struct AddExpr : public Expr {
+  explicit AddExpr(Expr* left, Expr* right)
+      : Expr(ExprType::kAddExpr), left(left), right(right) {}
+  Expr* left;
+  Expr* right;
+};
+
+struct VarRef : public Expr {
+  explicit VarRef(int offset) : Expr(ExprType::kVarRef), offset(offset) {}
+  int offset;
+};
+
+struct Assign : public Expr {
+  explicit Assign(VarRef* left, Expr* right)
+      : Expr(ExprType::kAssign), left(left), right(right) {}
+  VarRef* left;
+  Expr* right;
+};
+
+enum class StmtType {
+  kExpr,
+  kBlock,
+};
+
+struct Stmt {
+  explicit Stmt(StmtType type) : type(type) {}
+  StmtType type;
+};
+
+struct ExprStmt : public Stmt {
+  explicit ExprStmt(Expr* expr) : Stmt(StmtType::kExpr), expr(expr) {}
+  Expr* expr;
+};
+
+struct BlockStmt : public Stmt {
+  explicit BlockStmt(const std::vector<const Stmt*>& body)
+      : Stmt(StmtType::kBlock), body(body) {}
+  std::vector<const Stmt*> body;
+};
+
+struct State {
+  int vars[26] = {};
+};
+
+int interpret(State* state, const Expr* expr) {
+  switch (expr->type) {
+    case ExprType::kIntLit: {
+      return reinterpret_cast<const IntLit*>(expr)->value;
+    }
+    case ExprType::kAddExpr: {
+      auto add = reinterpret_cast<const AddExpr*>(expr);
+      int left = interpret(state, add->left);
+      int right = interpret(state, add->right);
+      return left + right;
+    }
+    default: {
+      std::fprintf(stderr, "unsupported expr type\n");
+      std::abort();
+    }
+  }
+}
+
+void interpret(State* state, const Stmt* stmt) {
+  switch (stmt->type) {
+    case StmtType::kExpr: {
+      interpret(state, reinterpret_cast<const ExprStmt*>(stmt)->expr);
+      break;
+    }
+    case StmtType::kBlock: {
+      auto block = reinterpret_cast<const BlockStmt*>(stmt);
+      for (size_t i = 0; i < block->body.size(); i++) {
+        interpret(state, block->body[i]);
+      }
+      break;
+    }
+  }
+}
+
+void test_interp() {
+  struct expr_test {
+    Expr* expr;
+    int expected;
+  } expr_tests[] = {
+      {new IntLit(123), 123},
+      {new AddExpr(new IntLit(123), new IntLit(456)), 579},
+      {nullptr, 0},
+  };
+  std::vector<size_t> failed;
+  for (size_t i = 0; expr_tests[i].expr != nullptr; i++) {
+    State state;
+    int result = interpret(&state, expr_tests[i].expr);
+    if (result == expr_tests[i].expected) {
+      fprintf(stderr, ".");
+    } else {
+      failed.push_back(i);
+      fprintf(stderr, "E");
+    }
+  }
+  fprintf(stderr, "\n");
+  if (failed.size()) {
+    fprintf(stderr, "Failed tests:");
+    for (size_t test : failed) {
+      fprintf(stderr, " %zu", test);
+    }
+    fprintf(stderr, "\n");
+  }
+}
+
+int main() { test_interp(); }
